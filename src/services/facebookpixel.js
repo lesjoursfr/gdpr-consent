16e68fc8ff32d41225ee0ec0ce00c00d0b082c0e
Adrien ERAUD <adrien@lebondrive.fr>
2021-01-20 19:49:58 +0100

    Fix Facebook Pixel

--- src/services/facebookpixel.js
+++ src/services/facebookpixel.js
@@ -7,9 +7,20 @@ export default () => ({
 	name: "Facebook Pixel",
 	uri: "https://www.facebook.com/policies/cookies/",
 	needConsent: true,
-	cookies: [],
+	cookies: ["datr", "fr", "reg_ext_ref", "reg_fb_gate", "reg_fb_ref", "sb", "wd", "x-src"],
 	js: function() {
 		"use strict";
+		var n;
+		if (window.fbq) {return;}
+		n = window.fbq = function() {
+			n.callMethod ?
+				n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+		};
+		if (!window._fbq) {window._fbq = n;}
+		n.push = n;
+		n.loaded = !0;
+		n.version = "2.0";
+		n.queue = [];
 		addScript("//connect.facebook.net/en_US/fbevents.js", "facebook-fbevents", function() {
 			// eslint-disable-next-line no-undef
 			fbq("init", GDPRConsentUser.fbPixelId);

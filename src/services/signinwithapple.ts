33d79c25314b4297c1595319d2d45f69d2648a69
Adrien ERAUD <e-adrien@users.noreply.github.com>
2024-02-05 18:28:39 +0100

    Add a signinwithappleMore option

--- src/services/signinwithapple.ts
+++ src/services/signinwithapple.ts
@@ -1,7 +1,7 @@
-import { ServiceInterface, ServiceLoader } from "../interfaces/index.js";
+import { GDPRConsentUser, ServiceInterface, ServiceLoader } from "../interfaces/index.js";
 import { addScript } from "../utils/index.js";
 
-export const signinwithapple = ((): ServiceInterface => {
+export const signinwithapple = ((user: GDPRConsentUser): ServiceInterface => {
   return {
     key: "signinwithapple",
     type: "other",
@@ -11,7 +11,17 @@ export const signinwithapple = ((): ServiceInterface => {
     lazyConsent: true,
     cookies: [],
     js: function () {
-      addScript("https://appleid.cdn-apple.com/appleauth/static/jsapi/appleid/1/en_US/appleid.auth.js");
+      if (typeof user.signinwithappleMore === "function") {
+        addScript(
+          "https://appleid.cdn-apple.com/appleauth/static/jsapi/appleid/1/en_US/appleid.auth.js",
+          undefined,
+          function () {
+            (user.signinwithappleMore as () => void)();
+          }
+        );
+      } else {
+        addScript("https://appleid.cdn-apple.com/appleauth/static/jsapi/appleid/1/en_US/appleid.auth.js");
+      }
     },
   };
 }) as ServiceLoader;
